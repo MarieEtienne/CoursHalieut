c44428365f6729266a90e29d3044287dc4881623
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2018-09-01 22:46:24 +0200

    Cours halieute v0.0.1

---
+++ exercices/intror_exercice1.R
@@ -0,0 +1,16 @@
+##########################################################################
+##### Ceci est une ligne de commentaires 
+##### parce qu eles commentaires c'est utile pour dire ce qu'on fait
+##### par exemple 
+#####
+#####  Script d'illustration pour la prise en main de RStudio
+###########################################################################
+
+## charger un jeu de données déjà disponible dans R
+data(iris)
+
+## illustrer l'aspect objet
+class(iris)
+
+class(iris$Sepal.Length)
+class(iris$Species)
